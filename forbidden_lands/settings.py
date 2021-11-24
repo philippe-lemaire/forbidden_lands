b3acce35f2cf9de209641402ca99317e258f372c
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-24 14:58:23 +0100

    fix to allowed host to run on heroku + debug false

--- forbidden_lands/settings.py
+++ forbidden_lands/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "forbidden-lands.herokuapp.com"]
 
 
 # Application definition
