878ac3e90a05f8511ab8982b985eccb92b8a7fb6
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-11-27 09:09:00 +0100

    with inn name. inn object complete.

--- adventuresites/inn_generator.py
+++ adventuresites/inn_generator.py
@@ -21,6 +21,45 @@ inn_table = {
     65: ("Drunk adventurers", "Dwarven strong ale", "Scouting thief"),
 }
 
+inn_name_table = {
+    11: ("The Third", "Lantern"),
+    12: ("The Little", "Swine"),
+    13: ("The Red", "Wheel"),
+    14: ("The Misty", "Stoop"),
+    15: ("The Bloody", "Sparrow"),
+    16: ("The Old", "Goat"),
+    21: ("The Golden", "Arrow"),
+    22: ("The Cold", "Pot"),
+    23: ("The Refreshing", "Lamb"),
+    24: ("The Good", "Maid"),
+    25: ("The Second", "Man"),
+    26: ("The Last", "Dragon"),
+    31: ("The Prancing", "Griffin"),
+    32: ("The Happy", "Boar"),
+    33: ("The Singing" "Barrel"),
+    34: ("The Rolling", "Bard"),
+    35: ("The Rumping", "Dog"),
+    36: ("The Wailing", "Horse"),
+    41: ("The Greedy", "Girl"),
+    42: ("The Round", "Wolf"),
+    43: ("The Flaming", "Bear"),
+    44: ("The Last ", "Ghost"),
+    45: ("The Silver", "Rat"),
+    46: ("The Black", "Jar"),
+    51: ("The Dead", "Mug"),
+    52: ("The Big", "Goblet"),
+    53: ("The Roaring", "Eagle"),
+    54: ("The Cheering", "Raven"),
+    55: ("The Humming", "Hammer"),
+    56: ("The Meagre", "Spike"),
+    61: ("The Fat", "Crow"),
+    62: ("The Thick", "Druid"),
+    63: ("The Round", "Knight"),
+    64: ("The Sweet", "Bandit"),
+    65: ("The Boisterous", "Wild Boar"),
+    66: ("The Grumpy", "Hunter"),
+}
+
 
 class Inn:
     def __init__(self):
@@ -50,5 +89,23 @@ class Inn:
                 candidate = key
         self.special_guest = inn_table.get(candidate)[2]
 
+        first_roll = r.roll("d66")
+        last_roll = r.roll("d66")
+        name_type = r.roll("d10")
+
+        for key in inn_name_table:
+            if key <= first_roll:
+                candidate = key
+            if key <= last_roll:
+                candidate_last = key
+        first = inn_name_table.get(candidate)[0]
+
+        if name_type % 2 == 0:
+            last = f"and {inn_name_table.get(candidate_last)[0]}"
+        else:
+            last = inn_name_table.get(candidate_last)[1]
+
+        self.name = f"{first} {last}"
+
     def __repr__(self):
-        return f"An inn with this oddity: {self.oddity}. Their specialty: {self.specialty}. The special guest: {self.special_guest}."
+        return f"An inn called {self.name} with this oddity: {self.oddity}. Their specialty: {self.specialty}. The special guest: {self.special_guest}."
